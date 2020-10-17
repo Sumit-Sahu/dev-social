7d76f1ad999aaa1d830e5f8064cc1cdef3d97f0d
sumit <sumiiitjsahu@gmail.com>
2020-10-17 10:07:36 +0530

    route files with express router

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/',(req,res) => {
     res.send("server running");
 });
 
+//Define routers
+app.use('/api/users',require('./routes/api/user'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
 port = process.env.port || 3000;
 
 app.listen(port,() => console.log(`listening on server ${port}`));
