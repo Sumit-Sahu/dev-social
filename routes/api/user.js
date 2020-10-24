01b1a4b1e0a6fb584ce23227d452d69a6720ab32
sumit <sumiiitjsahu@gmail.com>
2020-10-24 19:28:16 +0530

    request body validation

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router(); 
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
 
-router.get('/',(req, res) => res.send("user route"));
+router.post('/', [
+    body('name', 'Name is required')
+    .not()
+    .isEmpty(),
+    body('email','please include a valid email').isEmail(),
+    body('password','password should atleast 6 char').isLength({ min:6 })
+],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+    res.send("user route");
+});
 
 module.exports = router;
