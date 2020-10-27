30939233ed190b1ef40def7bf555182d82605810
sumit <sumiiitjsahu@gmail.com>
2020-10-27 13:32:37 +0530

    add and delete experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -150,4 +150,85 @@ router.delete('/', auth, async (req, res) => {
 
     
 });
+
+// @route   PUT api/proile/experience
+// @desc    Add profile experience
+// @access  private
+
+router.put('/experience',
+[
+    auth, 
+    [
+        body('title','title is requered')
+        .not()
+        .isEmpty(),
+        body('company','company is requered')
+        .not()
+        .isEmpty(),
+        body('from','from date is requered')
+        .not()
+        .isEmpty(),
+    ]
+
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description       
+    } = req.body
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description       
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id});
+        // console.log(profile);
+
+        profile.experience.unshift(newExp);
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ msg: 'Server Error'});
+    }
+});
+
+// @route   DELETE api/proile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  private
+
+router.delete('/experience/:exp_id', auth, async ( req, res ) => {
+    try {
+        let profile = await Profile.findOne({ user: req.user.id});
+        const index = profile.experience.map( exp => exp.id).indexOf(req.params.exp_id);
+        if ( index >=0)
+            profile.experience.splice(index,1);
+        
+        await profile.save();
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ msg: 'Server Error'});
+    }
+
+});
+
 module.exports = router;
