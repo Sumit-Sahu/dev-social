a74cfe2205360a747fbdf657d477dd129b8b0b68
sumit <sumiiitjsahu@gmail.com>
2020-10-27 10:02:02 +0530

    route for get all profile and profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -85,4 +85,46 @@ async (req, res) => {
     }
 });
 
+
+// @route   GET api/proile/me
+// @desc    Get current user profile
+// @access  private
+
+router.get('/', async (req, res) => {
+    try{
+        const profiles = await Profile.find().populate('user', ['name' , 'avatar']);
+        if(!profiles) return res.status(400).json({ msg: 'There sre no profiles'});
+
+        res.json(profiles);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server Error'});
+    }
+
+    
+});
+
+
+// @route   GET api/proile/user/:user_id
+// @desc    Get current user profile
+// @access  private
+
+router.get('/user/:user_id', async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name' , 'avatar']);
+        if(!profile) return res.status(400).json({ msg: 'Profile Not Found'});
+
+        res.json(profile);
+    }
+    catch(err){
+        console.error(err.message);
+        if(err.kind === 'ObjectId')
+            return res.status(400).json({ msg: 'Profile Not Found'});
+        res.status(500).json({ msg: 'Server Error'});
+    }
+
+    
+});
+
 module.exports = router;
