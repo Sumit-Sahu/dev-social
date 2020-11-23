6b7f4457a02527ad44bfcf053e5a81f49265e8ab
sumit <sumiiitjsahu@gmail.com>
2020-11-23 14:34:15 +0530

    load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,18 @@ import Alert from './components/layout/Alert';
 // Redux
 import {Provider} from 'react-redux';
 import store from './store'
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store = {store}>
       <Router>
